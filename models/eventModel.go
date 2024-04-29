ad865d266bc1dd7fec72a367d9d5ba2206a974df
Martin Hellström <martin@hellstrom.it>
2024-04-30 01:03:48 +0200

    fix json event

--- models/eventModel.go
+++ models/eventModel.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Event struct {
 	gorm.Model
-	PlayerID uint   `json:"PlayerID"`
-	Event    string `json:"Event"`
-	TargetID *uint  `json:"TargetID"`
-	WeaponID *uint  `json:"WeaponID"`
+	PlayerID uint   `json:"playerid"`
+	Event    string `json:"event"`
+	TargetID *uint  `json:"targetid"`
+	WeaponID *uint  `json:"weaponid"`
 }
