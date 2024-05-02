d90ebadefa730aaf51924469cb6f36e3ccd85e65
Martin Hellström <martin@hellstrom.it>
2024-05-02 20:59:53 +0200

    Initial working models

--- models/playerModel.go
+++ models/playerModel.go
@@ -1,12 +1,48 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Player struct {
-	gorm.Model
-	Name   string
-	UCID   string  `gorm:"unique"`
-	Events []Event `gorm:"foreignKey:PlayerID"`
+	PlayerID   uint32 `gorm:"unique;primaryKey"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	PlayerName *string
+	Unit       `gorm:"foreignKey:UnitID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	UCID       string `gorm:"unique;not null"`
+	IP         string
+	Id         uint32
+	UnitID     uint
+}
+
+func (p *Player) GetPlayerName() string {
+	if p != nil && p.PlayerName != nil {
+		return *p.PlayerName
+	}
+	return ""
+}
+
+func (p *Player) GetUCID() string {
+	if p != nil {
+		return p.UCID
+	}
+	return ""
+}
+
+func (p *Player) GetIP() string {
+	if p != nil {
+		return p.IP
+	}
+	return ""
+}
+
+func (p *Player) GetId() uint32 {
+	if p != nil {
+		return p.Id
+	}
+	return 0
 }
--- models/unitModel.go
+++ models/unitModel.go
@@ -1,10 +1,22 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/DCS-gRPC/go-bindings/dcs/v0/common"
+)
 
 type Unit struct {
-	gorm.Model
-	Type     string `gorm:"unique"`
-	Category string
-	Events   []Event `gorm:"foreignKey:TargetID"`
+	UnitID   uint `gorm:"primaryKey;autoIncrement;not null;unique;index"`
+	Id       uint32
+	Name     string
+	Callsign string
+	common.Coalition
+	Type          string
+	Position      *Position     `gorm:"foreignKey:UnitID"`
+	Group         *common.Group `gorm:"foreignKey:UnitID;references:Id"`
+	NumberInGroup uint32
+}
+
+type Position struct {
+	UnitID          uint `gorm:"primaryKey;index"`
+	common.Position `gorm:"embedded"`
 }
